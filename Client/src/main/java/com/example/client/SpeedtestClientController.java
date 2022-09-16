d6809c727d0188c5fd86441316e74986e1137c46
Hubert Krauza <hubertkrauza@gmail.com>
2022-09-16 14:28:25 +0200

    update

--- Client/src/main/java/com/example/client/SpeedtestClientController.java
+++ Client/src/main/java/com/example/client/SpeedtestClientController.java
@@ -7,17 +7,24 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
 public class SpeedtestClientController {
+    @FXML
     public Label bytesLabel;
+    @FXML
     public Label tcpLabel;
+    @FXML
     public Label udpLabel;
+    @FXML
+    public Label serverAddressLabel;
 
-
+    @FXML
     public Slider packetSizeSlider;
+    @FXML
     public TextField ipAddressField;
+    @FXML
     public TextField portField;
-    public Button addAddressButton;
-    public ComboBox<String> addressCombo;
+    @FXML
     public CheckBox nagleCheckBox;
+    @FXML
     public Button startStopButton;
 
     private boolean nagleAlgorithm = false;
@@ -26,11 +33,77 @@ public class SpeedtestClientController {
     private final BooleanProperty hasTCPConnected = new SimpleBooleanProperty(false);
     private final BooleanProperty hasUDPConnected = new SimpleBooleanProperty(false);
 
-    public void nagleAlgorithmCheckboxHandler(ActionEvent actionEvent) {
+    @FXML
+    public void initialize() {
+        this.packetSizeSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            this.bytesLabel.setText("Bytes: " + newValue.intValue());
+        });
+
+        this.hasTCPConnected.addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                this.tcpLabel.setText("TCP Thread: connected");
+            } else  {
+                this.tcpLabel.setText("TCP Thread: error");
+            }
+        });
+        this.hasUDPConnected.addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                this.udpLabel.setText("UDP Thread: connected");
+            } else {
+                this.udpLabel.setText("UDP Thread: error");
+            }
+        });
+
+    }
+
+    public void onNagleAlgorithmCheckbox(ActionEvent actionEvent) {
         this.nagleAlgorithm = !this.nagleAlgorithm;
     }
 
-    public void startStopHandler(ActionEvent actionEvent) {
-
+    public void onStartStop() {
+        this.isRunning = !this.isRunning;
+        this.refreshWindow();
     }
+
+    private void refreshWindow() {
+        String tcpLabel, udpLabel, buttonText;
+        if (this.isRunning) {
+            tcpLabel = "TCP Thread: starting";
+            udpLabel = "UDP Thread: starting";
+            buttonText = "Stop transmission";
+
+
+            this.serverAddressLabel.setText(ipAddressField.getText() + ":" + portField.getText());
+        } else {
+            tcpLabel = "TCP Thread: disconnected";
+            udpLabel = "UDP Thread: stopped";
+            buttonText = "Start transmission";
+
+//            if (BenchmarkApplication.tcpSender != null) {
+//                BenchmarkApplication.tcpSender.shutdown();
+//                try {
+//                    BenchmarkApplication.tcpSender.join();
+//                } catch (InterruptedException ignored) {
+//                }
+//            }
+//            if (BenchmarkApplication.udpSender != null) {
+//                BenchmarkApplication.udpSender.shutdown();
+//                try {
+//                    BenchmarkApplication.udpSender.join();
+//                } catch (InterruptedException ignored) {
+//                }
+//            }
+            this.serverAddressLabel.setText("None");
+        }
+
+        this.tcpLabel.setText(tcpLabel);
+        this.udpLabel.setText(udpLabel);
+        this.startStopButton.setText(buttonText);
+
+        this.packetSizeSlider.setDisable(this.isRunning);
+        this.ipAddressField.setDisable(this.isRunning);
+        this.portField.setDisable(this.isRunning);
+        this.nagleCheckBox.setDisable(this.isRunning);
+    }
+
 }
--- Server/src/main/java/com/example/server/SpeedtestServerController.java
+++ Server/src/main/java/com/example/server/SpeedtestServerController.java
@@ -1,14 +1,55 @@
 package com.example.server;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class SpeedtestServerController {
     @FXML
-    private Label welcomeText;
+    public TextField portField;
+    @FXML
+    public Button startStopButton;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public TextField tcp_sds;
+    @FXML
+    public TextField tcp_tsotd;
+    @FXML
+    public TextField tcp_ttt;
+    @FXML
+    public TextField tcp_sct;
+    @FXML
+    public TextField tcp_ts;
+    @FXML
+    public TextField tcp_ld;
+    @FXML
+    public TextField tcp_te;
+
+
+    @FXML
+    public TextField udp_sds;
+    @FXML
+    public TextField udp_tsotd;
+    @FXML
+    public TextField udp_ttt;
+    @FXML
+    public TextField udp_sct;
+    @FXML
+    public TextField udp_ts;
+    @FXML
+    public TextField udp_ld;
+    @FXML
+    public TextField udp_te;
+
+
+    @FXML
+    public void initialize() {
+
+    }
+
+    public void onStartStop(ActionEvent actionEvent) {
     }
 }
